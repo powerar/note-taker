b1daef03737be0b1032de184f411e84b0a864123
powerar <arpower@bell.net>
2021-05-06 18:13:46 -0400

    added main and note routes

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`API server running on ${PORT}`);
