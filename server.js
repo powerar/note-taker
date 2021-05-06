a520d93010dbecd833295e3079653a207e2142f9
powerar <arpower@bell.net>
2021-05-06 18:33:54 -0400

    notes.html style and save button loads

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 
+
+
 const app = express();
 
 const PORT = process.env.PORT || 3001;
@@ -11,6 +13,7 @@ app.get('/', (req, res) => {
 
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
+  app.use(express.static('public'));
 });
 
 app.listen(PORT, () => {
